92c585ce9ffc89c3e5d360d1d7aa435f7d7169ab
alex.cabrera1 <alex.cabrera@computrols.com>
2026-06-29 19:42:14 -0500

    updated readme

--- NewProject/JuceLibraryCode/JuceHeader.h
+++ NewProject/JuceLibraryCode/JuceHeader.h
@@ -40,7 +40,7 @@
 #if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
-    const char* const  projectName    = "Track Distance";
+    const char* const  projectName    = "Doppler Distance";
     const char* const  companyName    = "";
     const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
--- NewProject/JuceLibraryCode/JucePluginDefines.h
+++ NewProject/JuceLibraryCode/JucePluginDefines.h
@@ -41,13 +41,13 @@
  #define JucePlugin_Enable_ARA             0
 #endif
 #ifndef  JucePlugin_Name
- #define JucePlugin_Name                   "Track Distance"
+ #define JucePlugin_Name                   "Doppler Distance"
 #endif
 #ifndef  JucePlugin_Desc
- #define JucePlugin_Desc                   "Track Distance"
+ #define JucePlugin_Desc                   "Doppler Distance"
 #endif
 #ifndef  JucePlugin_Manufacturer
- #define JucePlugin_Manufacturer           "yourcompany"
+ #define JucePlugin_Manufacturer           "Alex Cabrera"
 #endif
 #ifndef  JucePlugin_ManufacturerWebsite
  #define JucePlugin_ManufacturerWebsite    "www.yourcompany.com"
@@ -101,19 +101,19 @@
  #define JucePlugin_AUSubType              JucePlugin_PluginCode
 #endif
 #ifndef  JucePlugin_AUExportPrefix
- #define JucePlugin_AUExportPrefix         TrackDistanceAU
+ #define JucePlugin_AUExportPrefix         DopplerDistanceAU
 #endif
 #ifndef  JucePlugin_AUExportPrefixQuoted
- #define JucePlugin_AUExportPrefixQuoted   "TrackDistanceAU"
+ #define JucePlugin_AUExportPrefixQuoted   "DopplerDistanceAU"
 #endif
 #ifndef  JucePlugin_AUManufacturerCode
  #define JucePlugin_AUManufacturerCode     JucePlugin_ManufacturerCode
 #endif
 #ifndef  JucePlugin_CFBundleIdentifier
- #define JucePlugin_CFBundleIdentifier     com.yourcompany.TrackDistance
+ #define JucePlugin_CFBundleIdentifier     com.yourcompany.DopplerDistance
 #endif
 #ifndef  JucePlugin_AAXIdentifier
- #define JucePlugin_AAXIdentifier          com.yourcompany.TrackDistance
+ #define JucePlugin_AAXIdentifier          com.yourcompany.DopplerDistance
 #endif
 #ifndef  JucePlugin_AAXManufacturerCode
  #define JucePlugin_AAXManufacturerCode    JucePlugin_ManufacturerCode
@@ -137,7 +137,7 @@
  #define JucePlugin_IAASubType             JucePlugin_PluginCode
 #endif
 #ifndef  JucePlugin_IAAName
- #define JucePlugin_IAAName                "yourcompany: Track Distance"
+ #define JucePlugin_IAAName                "Alex Cabrera: Doppler Distance"
 #endif
 #ifndef  JucePlugin_VSTNumMidiInputs
  #define JucePlugin_VSTNumMidiInputs       16
@@ -152,10 +152,10 @@
  #define JucePlugin_ARATransformationFlags  0
 #endif
 #ifndef  JucePlugin_ARAFactoryID
- #define JucePlugin_ARAFactoryID           "com.yourcompany.TrackDistance.factory"
+ #define JucePlugin_ARAFactoryID           "com.yourcompany.DopplerDistance.factory"
 #endif
 #ifndef  JucePlugin_ARADocumentArchiveID
- #define JucePlugin_ARADocumentArchiveID   "com.yourcompany.TrackDistance.aradocumentarchive.1.0.0"
+ #define JucePlugin_ARADocumentArchiveID   "com.yourcompany.DopplerDistance.aradocumentarchive.1.0.0"
 #endif
 #ifndef  JucePlugin_ARACompatibleArchiveIDs
  #define JucePlugin_ARACompatibleArchiveIDs  ""
